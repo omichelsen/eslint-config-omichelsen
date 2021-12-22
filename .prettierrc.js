24d76f9036b55e584f3c519430ec0edf05f75404
Ole Bjørn Michelsen <ole@michelsen.dk>
2021-12-22 14:06:41 -0800

    chore: upgrade to eslint 8

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,10 +1,10 @@
 module.exports = {
   arrowParens: 'always',
-  jsxBracketSameLine: false,
+  bracketSameLine: false,
   quoteProps: 'as-needed',
   semi: false,
   singleQuote: true,
   tabs: true,
   tabWidth: 2,
   trailingComma: 'es5',
-};
+}
